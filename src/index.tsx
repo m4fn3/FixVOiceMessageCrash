384c3603e4c7646b9e97b30106f89e713354c933
mafu <mafusukee@gmail.com>
2023-05-20 23:52:48 +0900

    First

--- src/index.tsx
+++ src/index.tsx
@@ -3,47 +3,30 @@ import {React} from 'enmity/metro/common'
 import {create} from 'enmity/patcher'
 // @ts-ignore
 import manifest, {name as plugin_name} from '../manifest.json'
-import {getStoreHandlers} from "../../hook"
 import {getByProps} from "enmity/modules"
-import {get} from "enmity/api/settings"
-import Settings from "./components/Settings"
 
-const Patcher = create('NoBlockedMessage')
+const Patcher = create('FixVoiceMessageCrashPatch')
+const ReactNative = getByProps("View")
+const { DCDChatManager } = ReactNative.NativeModules
 
-const RelationshipStore = getByProps("isBlocked", "getFriendIDs")
-const [
-    MessageStore
-] = [
-    getStoreHandlers("MessageStore")
-]
-
-const NoBlockedMessage: Plugin = {
+const FixVoiceMessageCrashPatch: Plugin = {
     ...manifest,
     onStart() {
-        Patcher.before(MessageStore, "LOAD_MESSAGES_SUCCESS", (self, args, res) => {
-            args[0].messages = args[0].messages.filter((message) => {
-                return !RelationshipStore.isBlocked(message?.author?.id)
-                    && !(message.referenced_message && get(plugin_name, "replies", true) && RelationshipStore.isBlocked(message.referenced_message.author?.id))
-            })
-        })
-        for (const event of ["MESSAGE_CREATE", "MESSAGE_UPDATE"]) {
-            Patcher.before(MessageStore, event, (self, args, res) => {
-                let message = args[0].message
-                if (
-                    RelationshipStore.isBlocked(message?.author?.id)
-                    || (message.referenced_message && get(plugin_name, "replies", true) && RelationshipStore.isBlocked(message.referenced_message.author?.id))
-                ) {
-                    args[0].message = {}
+        Patcher.before(DCDChatManager, "updateRows", (_, args, __) => {
+            const rows = JSON.parse(args[1])
+            for ( const row of rows ) {
+                if (row.message?.flags === 8192){
+                    row.message.flags = 0
+                    // row.message.attachments = []
+                    // row.message.content = "[VoiceMessage]"
                 }
-            })
-        }
+            }
+            args[1] = JSON.stringify(rows)
+        })
     },
     onStop() {
         Patcher.unpatchAll()
-    },
-    getSettingsPanel({settings}) {
-        return <Settings settings={settings}/>
     }
 }
 
-registerPlugin(NoBlockedMessage)
+registerPlugin(FixVoiceMessageCrashPatch)
